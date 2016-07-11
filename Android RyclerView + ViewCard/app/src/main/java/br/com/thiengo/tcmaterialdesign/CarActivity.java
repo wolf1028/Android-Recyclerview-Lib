fc74a708ff2fe6e56d564522d19cfa4b307073b5
Walter Rodríguez Castañeda <xxw4lt3rxx@gmail.com>
2016-07-11 06:47:53 -0500

    RecyclerView  Android Final
    RecyclerView + CardView + Lib and more

---
+++ Android RyclerView + ViewCard/app/src/main/java/br/com/thiengo/tcmaterialdesign/CarActivity.java
@@ -0,0 +1,131 @@
+package br.com.thiengo.tcmaterialdesign;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.internal.view.ContextThemeWrapper;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mikepenz.materialdrawer.Drawer;
+
+import br.com.thiengo.tcmaterialdesign.domain.Car;
+import me.drakeet.materialdialog.MaterialDialog;
+
+
+public class CarActivity extends ActionBarActivity {
+    private Toolbar mToolbar;
+    private Car car;
+    private Drawer.Result navigationDrawerLeft;
+    private MaterialDialog mMaterialDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car);
+
+        if(savedInstanceState != null){
+            car = savedInstanceState.getParcelable("car");
+        }
+        else {
+            if (getIntent() != null && getIntent().getExtras() != null && getIntent().getExtras().getParcelable("car") != null) {
+                car = getIntent().getExtras().getParcelable("car");
+            } else {
+                Toast.makeText(this, "Fail!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+        mToolbar = (Toolbar) findViewById(R.id.tb_main);
+        mToolbar.setTitle(car.getModel());
+        setSupportActionBar(mToolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+        ImageView ivCar = (ImageView) findViewById(R.id.iv_car);
+        TextView tvModel = (TextView) findViewById(R.id.tv_model);
+        TextView tvBrand = (TextView) findViewById(R.id.tv_brand);
+        TextView tvDescription = (TextView) findViewById(R.id.tv_description);
+        Button btPhone = (Button) findViewById(R.id.bt_phone);
+
+        btPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMaterialDialog = new MaterialDialog(new ContextThemeWrapper(CarActivity.this, R.style.MyAlertDialog))
+                        .setTitle("Telefono de la Empresa")
+                        .setMessage(car.getTel())
+                        .setPositiveButton("Llamar", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent it = new Intent(Intent.ACTION_CALL);
+                                it.setData(Uri.parse("tel:" + car.getTel().trim()));
+                                startActivity(it);
+                            }
+                        })
+                        .setNegativeButton("Cancelar", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mMaterialDialog.dismiss();
+                            }
+                        });
+                mMaterialDialog.show();
+            }
+        });
+
+        ivCar.setImageResource(car.getPhoto());
+        tvModel.setText(car.getModel());
+        tvBrand.setText(car.getBrand());
+        tvDescription.setText( car.getDescription() );
+
+        navigationDrawerLeft = new Drawer()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(false)
+            .withCloseOnClick(true)
+            .withActionBarDrawerToggleAnimated(false)
+                .withActionBarDrawerToggle(new ActionBarDrawerToggle(this, new DrawerLayout(this), R.string.drawer_open, R.string.drawer_close){
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        super.onDrawerSlide(drawerView, slideOffset);
+                        navigationDrawerLeft.closeDrawer();
+                        finish();
+                    }
+                })
+            .build();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("car", car);
+    }
+}
